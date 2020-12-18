1945877c3efcaa7e6948f698b74e87c633b4654b
Trond Sørås <trond@dx.no>
2020-12-18 08:52:01 +0100

    ignore target

--- day01/target/debug/deps/day01-173e33ffbfe32944.d
+++
@@ -1,5 +0,0 @@
-/Users/trond/projects/adventofcode2020/day01/target/debug/deps/day01-173e33ffbfe32944.rmeta: src/main.rs
-
-/Users/trond/projects/adventofcode2020/day01/target/debug/deps/day01-173e33ffbfe32944.d: src/main.rs
-
-src/main.rs:
